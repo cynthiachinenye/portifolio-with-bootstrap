c3955378c9bbd5532b4142bf7fe60d2b97222735
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-27 12:31:16 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
+  <Navbar/>
 </div>
   );
 }
