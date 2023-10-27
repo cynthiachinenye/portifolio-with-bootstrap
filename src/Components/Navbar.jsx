bbd3f4cd479910078b5215de44df15ebbc01309e
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-27 14:35:06 +0100

    new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,21 +4,39 @@ import logo from "../"
 const Navbar = () => {
   return (
     <div>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light  bg-dark">
+    <div class="container">
     <a className="navbar-brand" href="#"><h1 className='logo'> cyndy..</h1></a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon "></span>
     </button>
   
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
+      <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
           <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">about me</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">services</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">how work</a>
+        </li>
+
+        <li className="nav-item">
+          <a className="nav-link" href="#">portfolio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">contact</a>
         </li>
        </ul>
+    </div>
+
     </div>
   </nav>
   
