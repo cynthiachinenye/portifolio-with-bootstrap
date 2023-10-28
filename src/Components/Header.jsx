783987f5e34c45bd2f9f6553424d2c2c140fdbeb
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-28 15:37:26 +0100

    new

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Typed from "react-typed"
 
 const Header = () => {
   return (
     <div className='header-wrapper'>
      <div className='main-info'>
      <h1>web development and websites promotions</h1>
+     <Typed className="typed-text"
+     strings={{"web Design", "web Development", "Facebook Ads SMM", "Google Ads"}}/>
      </div>
       
     </div>
