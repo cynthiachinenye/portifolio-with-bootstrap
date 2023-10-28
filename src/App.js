38b3f429fce8936be6f1f5afbb9607cfb283f2c6
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-28 15:02:37 +0100

    new

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
   <Navbar/>
+  <Header/>
 </div>
   );
 }
---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+     <div className='main-info'>
+     <h1>web development and websites promotions</h1>
+     </div>
+      
+    </div>
+  )
+}
+
+export default Header
