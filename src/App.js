d94fa366fe259831fd54dd0848c25ee904d28a15
cynthiachinenye <cynthiachiagu@gmail.com>
2023-10-29 13:28:07 +0100

    new

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './Components/Navbar';
 import Header from './Components/Header';
 import Particles from 'react-tsparticles'
+import { useCallback } from 'react';
+import { loadSlim} from 'tsparticles-slim'
 
+ 
 function App() {
+  const particlesInit = useCallback(async engine =>{
+     console.log(engine)
+     await loadSlim(engine)
+  },[]);
+  const  particlesLoaded = useCallback(async container => {
+    await console.log(container)
+  },[])
   return (
     <div className="App">
     <Particles
-    className='particles-canvas'
-    params={{
+    init={particlesInit}
+    loaded={particlesLoaded}
+    id='tsparticles'
+    options={{
       particles:{
         number:{
           value: 30,
